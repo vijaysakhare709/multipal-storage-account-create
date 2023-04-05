6330d9b79ac244fed9b06fa08696c8917eff1ad3
vijaysakhare709 <129182134+vijaysakhare709@users.noreply.github.com>
2023-04-05 10:52:53 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  count = 3
+  name                     = "${count.index}storageaccountname"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  account_kind = "StorageV2"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
